d8403c7e0c1350d0e223b7476405668d0d9e410f
Felipe José <lipejose@icloud.com>
2022-01-09 11:33:33 -0300

    feat: user loader in middleware

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -2,20 +2,23 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Env from '@ioc:Adonis/Core/Env'
 import { PrismaClient } from '@prisma/client'
 import * as jwt from 'jsonwebtoken'
+import { UserLoader } from 'App/Loaders/User'
 
 export type ModifiedBody = { userId: string }
 
 export default class Auth {
   private client: PrismaClient
+  private loader: UserLoader
 
   constructor() {
     this.client = new PrismaClient()
+    this.loader = new UserLoader(this.client)
   }
 
   public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
     const token = <string>request.header('Authorization')?.replace('Bearer ', '')
     const decodedToken = jwt.verify(token, Env.get('APPLICATION_SECRET_KEY')) as { id: string }
-    const user = await this.client.user.findUnique({ where: { id: decodedToken.id } })
+    const user = await this.loader.load(decodedToken.id)
 
     if (user?.id) {
       request.updateBody({ userId: user?.id, ...request.body() })
